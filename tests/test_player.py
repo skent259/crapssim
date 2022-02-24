5d51d8188bfe9b934f3d7d8f35afcea6b4e1d4f1
Corey <coreyronning@gmail.com>
2022-02-24 10:34:51 -0600

    Changed player so if a bet is added after there is already the same bet on the table it just adds the wager amount to the bet instead of adding a whole new bet to the table.

--- tests/test_player.py
+++ tests/test_player.py
@@ -9,6 +9,21 @@ def test_default_strategy():
     assert player.bet_strategy == passline
 
 
+def test_existing_bet():
+    player = Player(100)
+    bet_one = PassLine(50)
+    player.bet(bet_one)
+    bet_two = PassLine(50)
+    player.bet(bet_two)
+
+    bet_count = len(player.bets_on_table)
+    bet_amount = player.bets_on_table[0].bet_amount
+    bankroll = player.bankroll
+    total_bet_amount = player.total_bet_amount
+
+    assert (bet_count, bet_amount, bankroll, total_bet_amount) == (1, 100, 0, 100)
+
+
 def test_irremovable_bet():
     player = Player(100)
     bet = PassLine(50)
