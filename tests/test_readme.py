0017da06252bf365787a6551fe11b07ac28b3472
Corey <coreyronning@gmail.com>
2022-05-11 08:59:22 -0500

    Commented out test_second_chunk as it had dramatically increased testing time from 21ms to about 2 seconds.

--- tests/test_readme.py
+++ tests/test_readme.py
@@ -11,19 +11,19 @@ def test_first_chunk():
     table.run(max_rolls=20, verbose=False)
 
 
-def test_second_chunk():
-    n_sim = 10
-    bankroll = 300
-    strategies = {
-        "place68": craps.strategy.place68,
-        "ironcross": craps.strategy.ironcross
-    }
-
-    for i in range(n_sim):
-        table = craps.Table()
-        for s in strategies:
-            table.add_player(craps.Player(bankroll, strategies[s], s))
-
-        table.run(max_rolls=float("inf"), max_shooter=10, verbose=False)
-        for s in strategies:
-            print(f"{i}, {s}, {table.get_player(s).bankroll}, {bankroll}, {table.dice.n_rolls}")
+# def test_second_chunk():
+#     n_sim = 10
+#     bankroll = 300
+#     strategies = {
+#         "place68": craps.strategy.place68,
+#         "ironcross": craps.strategy.ironcross
+#     }
+#
+#     for i in range(n_sim):
+#         table = craps.Table()
+#         for s in strategies:
+#             table.add_player(craps.Player(bankroll, strategies[s], s))
+#
+#         table.run(max_rolls=float("inf"), max_shooter=10, verbose=False)
+#         for s in strategies:
+#             print(f"{i}, {s}, {table.get_player(s).bankroll}, {bankroll}, {table.dice.n_rolls}")
