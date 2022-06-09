4e10bc9da9a0d8cb266e62c6e751192a08aa0f7b
Corey <coreyronning@gmail.com>
2022-06-09 09:37:36 -0500

    Made it so bankroll can't go negative for strategies that have more than one bet.

--- crapssim/table.py
+++ crapssim/table.py
@@ -309,8 +309,9 @@ class Player:
     def add_bet(self, bet: Bet) -> None:
         existing_bets: list[Bet] = bet.already_placed_bets(self)
         new_bet = sum(existing_bets + [bet])
+        amount_available_to_bet = self.bankroll + sum(x.bet_amount for x in existing_bets)
 
-        if new_bet.allowed(self) and new_bet.bet_amount <= self.bankroll + bet.bet_amount:
+        if new_bet.allowed(self) and new_bet.bet_amount <= amount_available_to_bet:
             for bet in existing_bets:
                 self.bets_on_table.remove(bet)
             self.bankroll -= bet.bet_amount
