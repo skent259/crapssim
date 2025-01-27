b1aa0fae62d05ba453fe8582c1c50db6f25c1024
Sean Kent <skent259@gmail.com>
2025-01-27 08:21:44 -0600

    Make players take deepcopy of strategy
    * Fixes Simple Bets and BetIfTrue not working on Bets with persistent features (on multi sims) #48
    * Add testing to capture this working correctly on Fire bet strategy example

--- crapssim/table.py
+++ crapssim/table.py
@@ -1,3 +1,4 @@
+import copy
 import typing
 
 from crapssim.dice import Dice
@@ -346,7 +347,7 @@ class Player:
         name: str = "Player",
     ):
         self.bankroll: float = float(bankroll)
-        self.strategy: Strategy = bet_strategy
+        self.strategy: Strategy = copy.deepcopy(bet_strategy)
         self.name: str = name
         self.bets: list[Bet] = []
         self._table: Table = table
